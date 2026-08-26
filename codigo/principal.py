98f4c9423919ee3c50e52b4c80b60665878f69f2
Francisco Agostino <fagos2005@gmail.com>
2026-08-26 12:57:57 -0300

    Add introductory message to principal.py

--- codigo/principal.py
+++ codigo/principal.py
@@ -2,3 +2,4 @@ print("Primer programa del Proyecto Integrador")
 
 print("Integrantes:")
 print("- Agostino, Francisco \n- Benites Mateo Gaspar\n- Tobar, Thiago Ignacio\n- Perez Torres, Stefany Nicole")
+print("A continuacion el trabajo pactado")
