c92dfd0b49ae208fc4caf8be3b20f8f1e97305b7
Mateo Gaspar Benites <mbenites@buenosaires.gob.ar>
2026-08-26 12:48:26 -0300

    Agrega a los integrantes

--- codigo/principal.py
+++ codigo/principal.py
@@ -1,1 +1,4 @@
 print("Primer programa del Proyecto Integrador")
+
+print("Integrantes:")
+print("- Agostino, Francisco \n- Benites Mateo Gaspar\n- Tobar, Thiago Ignacio\n- Perez Torres, Stefany Nicole")
